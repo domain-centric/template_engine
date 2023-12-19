b8e45f6fd15dd96faa2732e7c1e64c10ef00e39e
Nils ten Hoeve <nilstenhoeve@gmail.com>
2023-12-19 07:46:31 +0100

    Fixed #203

--- test/src/parser/tag/expression/function/import/import_pure_test.dart
+++ test/src/parser/tag/expression/function/import/import_pure_test.dart
@@ -3,7 +3,7 @@ import 'package:template_engine/template_engine.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('The import function should import a file as is', () {
+  test('The importPure function should import a file as is', () {
     var engine = TemplateEngine();
     var parseResult = engine.parseText(
         "{{importPure('test/src/template_engine_template_example_test.dart')}}");
@@ -19,5 +19,18 @@ void main() {
         "  renderResult.text.should.be('Hello world.');\n"
         "}\n";
     renderResult.text.should.be(expected);
+  });
+
+  test('test importPure with none existing file', () {
+    var engine = TemplateEngine();
+    var parseResult = engine.parseText("{{importPure('none_existent.dart')}}");
+    var renderResult = engine.render(parseResult);
+    renderResult.text.should.be('{{ERROR}}');
+    renderResult.errorMessage.should
+        .contain("Render error in: '{{importPure('none_existent.dart')}}':");
+    renderResult.errorMessage.should
+        .contain("  1:3: Error importing a pure file: PathNotFoundException: "
+            "Cannot open file, path = ");
+    renderResult.errorMessage.should.contain("none_existent.dart");
   });
 }
